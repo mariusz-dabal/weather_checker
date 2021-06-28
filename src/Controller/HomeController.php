0d6daebc37bf2fd657f18881771ca91d8f1de462
Mariusz Dabal <mariusz.dabal@grupa.it>
2021-06-28 19:10:16 +0000

    Add Form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Location;
 use App\Form\WeatherLocationFormType;
+use App\Repository\LocationRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,12 +16,37 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $em, Request $request): Response
     {
         $form = $this->createForm(WeatherLocationFormType::class);
 
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /**@var Location $location */
+            $location = $form->getData();
+
+            $em->persist($location);
+            $em->flush();
+
+            $this->addFlash('success', 'Data submitted');
+
+            return $this->redirectToRoute('location');
+        }
+
         return $this->render('home/index.html.twig', [
             'locationForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/location", name="location")
+     */
+    public function list(LocationRepository $locationRepo) 
+    {
+        $locations = $locationRepo->findAll();
+
+        return $this->render('location/list.html.twig', [
+            'locations' => $locations,
         ]);
     }
 }
