570c6e486a2b5798ddd72c7f28a9cdf6637b81f7
Mariusz Dabal <mariusz.dabal@grupa.it>
2021-07-02 07:46:52 +0000

    fix checking date

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use App\Form\WeatherLocationFormType;
 use App\Repository\WeatherRepository;
 use App\Service\WeatherApi\WeatherService;
+use DateInterval;
+use DateTime;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,12 +31,24 @@ class HomeController extends AbstractController
             $weather = $form->getData();
 
             $city = strtolower($weather->getCity());
+            $currentTime = new DateTime('now');
 
-            if ($weatherRepo->findOneBy(['city' => $city])) {
+            if ($weatherRepo->findOneBy(['city' => $city]) &&
+                $weatherRepo->findOneBy(['city' => $city])->getCreatedAt()->add(new DateInterval('PT5M')) > $currentTime
+            ) {
                 $weather = $weatherRepo->findOneBy(['city' => $city]);
             } else {
                 try {
                     $weather = $weatherService->getWeather($city);
+
+                    if ($weatherRepo->findOneBy(['city' => $city])) {
+                        $oldWeather = $weatherRepo->findOneBy(['city' => $city]);
+                        $oldWeather->setTemperature($weather->getTemperature());
+                        $oldWeather->setDescription($weather->getDescription());
+                        $oldWeather->setCreatedAt($weather->getCreatedAt());
+                    } else {
+                        $em->persist($weather);
+                    }
                 } catch (\Exception $error) {
                     $this->addFlash('fail', 'Location Not Found!');
 
@@ -41,8 +56,7 @@ class HomeController extends AbstractController
                         'locationForm' => $form->createView(),
                     ]);
                 }
-                
-                $em->persist($weather);
+
                 $em->flush();
             }
 
